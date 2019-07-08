950a36699a7856253d12eb4f9b0399cd568cb932
Alvaro Garcia <agd3101@gmail.com>
2019-07-08 19:29:36 +0200

    08/07 añadido busqueda por familia

--- controllers/comic.controller.js
+++ controllers/comic.controller.js
@@ -19,12 +19,35 @@ module.exports.showComic = (req, res, next) => {
     .catch(next)
 }
 
+module.exports.showComicAmericano = (req, res, next) => {
+    Comic.find({family:{$in:["Americano"]}})
+.sort({ title: 1 })
+    .then(comics => res.status(200).json(comics))
+    .catch(next)
+  }
+
+  module.exports.showComicEuropeo = (req, res, next) => {
+    Comic.find({family:{$in:["Europeo"]}})
+.sort({ title: 1 })
+    .then(comics => res.status(200).json(comics))
+    .catch(next)
+  }
+
+  module.exports.showComicManga = (req, res, next) => {
+    Comic.find({family:{$in:["Manga"]}})
+.sort({ title: 1 })
+    .then(comics => res.status(200).json(comics))
+    .catch(next)
+  }
+
+
+
 
 module.exports.searchComic=(req,res,next) => {
 const {tags,family,finished, rating, title}=req.body
 const criteria = {};
 
-// if (title) { criteria.title = title }
+
 if (title) { criteria.title = new RegExp(title, 'i') }
 if (tags) { criteria.tags = {$all:tags} }
 if (family) { criteria.family = {$in:family} }
--- routes/comic.route.js
+++ routes/comic.route.js
@@ -5,8 +5,12 @@ const secure = require('../middlewares/secure.mid')
 const comicController = require('../controllers/comic.controller')
 
 router.get('/',secure.isAuthenticated,comicController.list) //hecho
+router.get('/americano',secure.isAuthenticated,comicController.showComicAmericano)
+router.get('/europeo',secure.isAuthenticated,comicController.showComicEuropeo)
+router.get('/manga',secure.isAuthenticated,comicController.showComicManga)
 router.get('/:id',secure.isAuthenticated,comicController.showComic)//hecho
 router.post('/search',secure.isAuthenticated,comicController.searchComic)
+
 
 
 
